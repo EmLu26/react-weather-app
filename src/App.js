58d036d7c29592a8322f27cb9d5ab1ac7d40d959
EmLu26 <133606715+EmLu26@users.noreply.github.com>
2023-11-04 19:24:38 +0000

    Added Bootstrap to project

--- src/App.js
+++ src/App.js
@@ -5,28 +5,30 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      {" "}
-      <h1> Weather App </h1>
-      <footer>
-        This project was coded with 💙 by{" "}
-        <a
-          href="https://www.shecodes.io/graduates/79171-emma-whitney"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Emma Whitney
-        </a>{" "}
-        and is
-        <a
-          href="https://github.com/EmLu26/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on GitHub.{" "}
-        </a>
-      </footer>
+      <div className="container">
+        {" "}
+        <h1> Weather App </h1>
+        <footer>
+          This project was coded with 💙 by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/79171-emma-whitney"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Emma Whitney
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/EmLu26/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub.{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
