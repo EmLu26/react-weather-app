d35d574faf8b1a724721487a4226e63d249802a6
EmLu26 <133606715+EmLu26@users.noreply.github.com>
2023-11-05 19:28:47 +0000

    Added useStates, axios and API

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         {" "}
-        <Weather />
+        <Weather defaultCity="New York" />
         <footer>
           This project was coded with 💙 by{" "}
           <a
