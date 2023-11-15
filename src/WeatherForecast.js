12cab520f1ede23de550ebff8f30541aa8060409
EmLu26 <133606715+EmLu26@users.noreply.github.com>
2023-11-15 22:28:56 +0000

    Added Forecast API (coords)

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,7 +1,20 @@
 import React from "react";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  console.log(props);
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "8d9838178b5b401f1b4e7cb5af18e210";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
