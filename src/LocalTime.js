d09accbe52239ff5fb1a082b36de6159a80718c5
EmLu26 <133606715+EmLu26@users.noreply.github.com>
2023-11-19 20:41:50 +0000

    Tided up LocalTime spacing

--- src/LocalTime.js
+++ src/LocalTime.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function LocalTime(props) {
   return (
     <div>
-      <span>Local time: GMT {props.timezone / 3600} :00</span>
+      <span>Local time: GMT {props.timezone / 3600}:00</span>
     </div>
   );
 }
